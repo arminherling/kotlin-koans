23a90ef2d8c61f86ea365f99d87e2ceb12108a7a
Armin Herling <armin.herling@gmail.com>
2017-11-16 21:02:21 +0100

    task 3: changed formatting

--- src/i_introduction/_3_Default_Arguments/n03DefaultArguments.kt
+++ src/i_introduction/_3_Default_Arguments/n03DefaultArguments.kt
@@ -25,5 +25,8 @@ fun foo(name: String, number: Number = 42, toUpperCase: Boolean = false): String
 
 fun task3(): String
 {
-    return (foo("a") + foo("b", number = 1) + foo("c", toUpperCase = true) + foo(name = "d", number = 2, toUpperCase = true))
+    return (foo("a") +
+            foo("b", number = 1) +
+            foo("c", toUpperCase = true) + 
+            foo(name = "d", number = 2, toUpperCase = true))
 }
