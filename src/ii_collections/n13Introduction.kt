9c29eb11c50207a76e2aec9bcd05541166bfb1da
Armin Herling <armin.herling@gmail.com>
2017-11-17 22:31:51 +0100

    task 13: introductions to collections

--- src/ii_collections/n13Introduction.kt
+++ src/ii_collections/n13Introduction.kt
@@ -10,15 +10,16 @@ import java.util.*
 /*
  *  There are many operations that help to transform one collection into another, starting with 'to'
  */
-fun example0(list: List<Int>) {
+fun example0(list: List<Int>)
+{
     list.toSet()
 
     list.toCollection(HashSet<Int>())
 }
 
-fun Shop.getSetOfCustomers(): Set<Customer> {
+fun Shop.getSetOfCustomers(): Set<Customer>
+{
     // Return a set containing all the customers of this shop
-    todoCollectionTask()
-//    return this.customers
+    return this.customers.toSet()
 }
 
