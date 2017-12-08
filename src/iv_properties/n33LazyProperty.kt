f7eca2c7ba52933745a3879aff375e92fec46f67
Armin Herling <armin.herling@gmail.com>
2017-12-08 23:08:52 +0100

    task 33: revisited lazy property and changed it to not use a delegated property

--- src/iv_properties/n33LazyProperty.kt
+++ src/iv_properties/n33LazyProperty.kt
@@ -2,18 +2,21 @@ package iv_properties
 
 import util.TODO
 
-class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int by lazy { initializer() }
+class LazyProperty(val initializer: () -> Int)
+{
+    val lazy: Int = 0
+        get()
+        {
+            if (field == 0) field = initializer()
+            return field
+        }
 }
 
-fun todoTask33(): Nothing = TODO(
-    """
+fun todoTask33(): Nothing = TODO("""
         Task 33.
         Add a custom getter to make the 'lazy' val really lazy.
         It should be initialized by the invocation of 'initializer()'
         at the moment of the first access.
         You can add as many additional properties as you need.
         Do not use delegated properties yet!
-    """,
-    references = { LazyProperty({ 42 }).lazy }
-)
+    """, references = { LazyProperty({ 42 }).lazy })
